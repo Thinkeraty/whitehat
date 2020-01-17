e4cb539a7ec9f7fb320c182984fb2ac4a1327718
Thinkeraty <contact.thinkeraty@gmail.com>
2020-01-17 18:51:32 +0530

    improvements

--- friend-bird-2/sketch.js
+++ friend-bird-2/sketch.js
@@ -99,3 +99,21 @@ function keyPressed(){
        slingshot.attach(bird.body);
     }
 }
+
+async function getBackgroundImg(){
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    var responseJSON = await response.json();
+
+    var datetime = responseJSON.datetime;
+    var hour = datetime.slice(11,13);
+    
+    if(hour>=0600 && hour<=1900){
+        bg = "sprites/bg.png";
+    }
+    else{
+        bg = "sprites/bg.jpg";
+    }
+
+    backgroundImg = loadImage(bg);
+    console.log(backgroundImg);
+}

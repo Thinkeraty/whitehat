5ba5dfe7d4f0032b76139e81f568e31a78430281
Thinkeraty <contact.thinkeraty@gmail.com>
2020-02-03 18:53:48 +0530

    images

--- car-race-2.5/sketch.js
+++ car-race-2.5/sketch.js
@@ -13,12 +13,12 @@ var cars, car1, car2, car3, car4;
 var track, car1_img, car2_img, car3_img, car4_img;
 
 function preload(){
-  track = loadImage("../images/track.jpg");
-  car1_img = loadImage("../images/car1.png");
-  car2_img = loadImage("../images/car2.png");
-  car3_img = loadImage("../images/car3.png");
-  car4_img = loadImage("../images/car4.png");
-  ground = loadImage("../images/ground.png");
+  track = loadImage("https://thinkeraty.github.io/whitehat/car-race-2.5/images/track.jpg");
+  car1_img = loadImage("https://thinkeraty.github.io/whitehat/car-race-2.5/images/car1.png");
+  car2_img = loadImage("https://thinkeraty.github.io/whitehat/car-race-2.5/images/car2.png");
+  car3_img = loadImage("https://thinkeraty.github.io/whitehat/car-race-2.5/images/car3.png");
+  car4_img = loadImage("https://thinkeraty.github.io/whitehat/car-race-2.5/images/car4.png");
+  ground = loadImage("https://thinkeraty.github.io/whitehat/car-race-2.5/images/ground.png");
 }
 
 function setup(){

58011b15b46189a88f3fe9ba98a59c71ea9e66c6
Thinkeraty <contact.thinkeraty@gmail.com>
2020-02-23 14:29:30 +0530

    shpaes-shooter

---
+++ shapes-shooter/player.js
@@ -0,0 +1,73 @@
+//Coded By Samyak Jain With <3 For WhiteHat Jr.
+
+var p;
+var size;
+var heading;
+var pVel;
+var force;
+var boostColor;
+var touch;
+var playerColor;
+var Score;
+var Health;
+
+function updatePlayer() {
+    boostColor = color(0)
+        //move and rotate player 
+
+    if (keyIsDown(LEFT_ARROW)) {
+        heading -= 6;
+    }
+    if (keyIsDown(RIGHT_ARROW)) {
+        heading += 6;
+    }
+    if (keyIsDown(UP_ARROW)) {
+        force = p5.Vector.fromAngle(radians(heading));
+        pVel.add(force.mult(0.2));
+        boostColor = color(0, 255, 0);
+    }
+
+    //contain player
+    if (p.x > 400) {
+        p.x = 0
+    }
+    if (p.x < 0) {
+        p.x = 400
+    }
+    if (p.y > 400) {
+        p.y = 0
+    }
+    if (p.y < 0) {
+        p.y = 400
+    }
+
+
+
+    //update player location
+    pVel.mult(0.978);
+    p.add(pVel);
+    //draw the player
+    push();
+    translate(p.x, p.y);
+    rotate(radians(heading));
+    //flame stuff
+
+    fill(boostColor)
+    triangle(-size + 2, size * .7, -size * 3.5, size / 7, size * -.7, -size * .7);
+    //health
+
+
+    fill(playerColor);
+    triangle(-size + 1, -size + 1, size + 1, 0, -size + 1, size + 1);
+    pop();
+    fill(255);
+    textSize(15);
+    text("Health: "+Health, 100 ,25)
+
+    fill(255);
+    text("Score: "+Score, 25, 25);
+
+    fill(0, 255, 0);
+    text("//Shapes Shooter By Samyak Jain", 170, 390);
+
+}

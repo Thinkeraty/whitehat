afe575b3e6b1285265f24a542479c93f50191e2a
Thinkeraty <contact.thinkeraty@gmail.com>
2020-03-23 18:56:08 +0530

    almost

--- Carbon/solar/Cloud.js
+++ Carbon/solar/Cloud.js
@@ -5,17 +5,27 @@ class Cloud {
         this.width = width;
         this.height = height;
 
-        this.image = loadImage("../assets/wind.png");
+        this.image = loadImage("../assets/cloud.png");
         this.body = createSprite(this.x, this.y, this.width, this.height);
+        //this.body.debug = true;
+
+        this.body.setCollider("rectangle", 0, 0, 180, 140);
+        this.body.debug = true;
+        this.body.x = this.x + 50;
+        this.body.y = this.y + 50;
+        this.body.width = this.width + 20;
+        this.body.height = this.height + 20;
+        
+        this.body.addImage(this.image);
+
+        this.body.scale = 0.25;
+
     }
     display() {
-        image(this.image, this.x, this.y, this.width, this.height);
-        this.body.visible = false;
-        this.body.x = this.x + 25;
-        this.body.y = this.y + 15;
-        this.body.width = this.width;
-        this.body.height = this.height;
+        //this.body.visible = false;
+
         
+
         drawSprites();
     }
 }
--- Carbon/solar/sketch.js
+++ Carbon/solar/sketch.js
@@ -3,85 +3,126 @@ function preload() {
   bulb_glow = loadImage("https://thinkeraty.github.io/whitehat/Carbon/assets/bulb-glow.png");
   bulb_unglow = loadImage("https://thinkeraty.github.io/whitehat/Carbon/assets/bulb.png");
 
-  bg_img = loadImage("../assets/wind_bg.jpg");
+  bg_img = loadImage("../assets/game_bg.png");
 
- 
+  sun_img = loadImage("../assets/sun.png");
+
+  shadow = loadImage("../assets/shadow.png");
 }
 function setup() {
-  createCanvas(800,400);
+  createCanvas(800, 500);
 
-  bulb = createSprite(50, 330, 50, 50);
+  bulb = createSprite(50, 430, 50, 50);
   bulb.addImage("unglowing", bulb_unglow);
   bulb.addImage("glowing", bulb_glow);
   bulb.scale = 0.15;
 
-  solarPanel = new Solar(200, 300, -40, -40);
+  solarPanel = new Solar(200, 400, -40, -40);
 
-  //cloud = new Cloud(350, 30, 50, 50);
+  cloud1 = new Cloud(200, 80, 120, 80);
+  cloud2 = new Cloud(250, 150, 120, 80);
 
-  cloud_y = Math.round(random(20, 310));
+  sun = createSprite(400, 50, 40, 40);
+  sun.addImage(sun_img);
+  sun.scale = 0.20;
 
-  cloud = new Cloud(800, 30, 60, 50);
+  cloud1_speed = Math.round(random(5, 8));
+  cloud2_speed = Math.round(random(-8, -5));
 
+    cloud1_shadow_x = cloud1.body.x;
+  cloud2_shadow_x = cloud2.body.x;
 
-  col = false;
-
+  cloud1_shadow_y = cloud1.body.y + 200;
+  cloud2_shadow_y = cloud2.body.y + 170;
 
 }
 
 function draw() {
   background(bg_img);  
 
-  
-
   solarPanel.x = mouseX;
 
   stroke("#ffffff");
   strokeWeight(5);
-  line(solarPanel.x + 176.5, solarPanel.y, solarPanel.x  + 176.5, 370);
+  line(solarPanel.x + 176.5, solarPanel.y + 30, solarPanel.x  + 176.5, 470);
+
+  console.log(mouseY);
 
   solarPanel.y = 240;
 
   solarPanel.x = mouseX + 350;
 
-  //cloud.display();
-
-  cloud.x = cloud.x - 5;
-
-  if(cloud.x < 0) {
-    cloud.x = 800;
-    cloud_y = Math.round(random(20, 250));
-    cloud.y = cloud_y;
-  }
-
   bulb.x = solarPanel.x - 20;
+
+  //col(solarPanel, sun)
 
   solarPanel.display();
 
-  console.log(solarPanel.fake.isTouching(cloud.body));
+  //collision();
 
-  collision();
+  
+
+  cloud1.body.velocityX = cloud1_speed;
+  cloud2.body.velocityX = cloud2_speed;
+
+  if(cloud1.body.x > width) {
+    cloud1_speed = Math.round(random(-8, -5));
+  }
+
+  if(cloud1.body.x < 0) {
+    cloud1_speed = Math.round(random(5, 8));
+  }
+
+  if(cloud2.body.x > width) {
+    cloud2_speed = Math.round(random(-8, -5));
+  }
+
+  if(cloud2.body.x < 0) {
+    cloud2_speed = Math.round(random(5, 8));
+  }
+
+  console.log(cloud1.x);
+
+  cloud1_shadow_x = cloud1.body.x;
+  cloud2_shadow_x = cloud2.body.x;
+
+  cloud1_shadow_y = cloud1.body.y + 200;
+  cloud2_shadow_y = cloud2.body.y + 170;
+
+
+  cloud1.display();
+  cloud2.display();
 
   drawSprites();
 
+  tint(255, 125);
+  image(shadow, cloud1_shadow_x - 80, cloud1_shadow_y - 160, 200, 340);
+  //tint(255, 125);
+  //image(shadow, cloud1_shadow_x - 80, cloud1_shadow_y - 160, 200, 340);
+  
 
-  cloud.display();
-
+ // image(shadow, cloud2_shadow_x - 80, cloud2_shadow_y - 130, 200, 270);
+  tint(255, 125);
+  image(shadow, cloud2_shadow_x - 80, cloud2_shadow_y - 130, 200, 270);
 }
 
 
 function collision() {
-  if(solarPanel.fake.isTouching(cloud.body) == false) {
+  if(solarPanel.x - cloud1_shadow_x < 200 / 2 + solarPanel.width / 2 &&
+    cloud1_shadow_x - solarPanel.x < 200 / 2 + solarPanel.width / 2) {
 
       bulb.changeImage("glowing", bulb_glow);
       bulb.scale = 0.25;
 
-      solarPanel.gif.position(solarPanel.fake.x - 70, solarPanel.fake.y - 70);
+      solarPanel.animation = solarPanel.image_glow;
+
+      console.log("hi");
 
 } else {
       bulb.changeImage("unglowing", bulb_unglow);
       bulb.scale = 0.15;
 
-      solarPanel.gif.position(-1000, -1000);
+      solarPanel.animation = solarPanel.image_unglow;
+
 }
 }
